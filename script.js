b27d033eb51ea8347b682eb740565ff5f41a4e47
Victor Nunes <urubuis1@gmail.com>
2022-08-19 10:39:36 -0400

    adicionada função para criar grid de X elementos

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-	for (let j = 0; j < 16; j++) {
-		let newDiv = document.createElement("div");
-		newDiv.classList.add("square");
-		container.appendChild(newDiv);
+generateDiv(16);
+
+function removeAllChildNodes(parent) {
+	while (parent.firstChild) {
+		parent.removeChild(parent.firstChild);
 	}
 }
+
+function generateDiv(numberSquares) {
+	removeAllChildNodes(container);
+	for (let i = 0; i < numberSquares; i++) {
+		for (let j = 0; j < numberSquares; j++) {
+			let newDiv = document.createElement("div");
+			newDiv.classList.add("square");
+			container.style.gridTemplateColumns = `repeat(${numberSquares}, 1fr)`;
+			container.appendChild(newDiv);
+		}
+	}
+}
+const gridElements = document.querySelectorAll(".square");
+
+gridElements.forEach((element) => {
+	element.addEventListener("mouseover", () => {
+		element.style.backgroundColor = "red";
+	});
+});
+
+const newGridBtn = document.querySelector(".newGrid");
+newGridBtn.addEventListener("click", () => {
+	let value = prompt("Input new grid value");
+	generateDiv(value);
+});
