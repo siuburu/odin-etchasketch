052064ee01e9dd4dc41cf544b41db827cbd2dcfb
Victor Nunes <urubuis1@gmail.com>
2022-08-19 11:25:17 -0400

    Adicionado slider para setar o número de quadrados

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
-generateDiv(16);
-
+let gridElements = document.querySelectorAll(".square");
+const gridSlider = document.querySelector(".gridSlider");
+generateDiv(gridSlider.value);
 function removeAllChildNodes(parent) {
 	while (parent.firstChild) {
 		parent.removeChild(parent.firstChild);
@@ -8,7 +9,7 @@ function removeAllChildNodes(parent) {
 }
 
 function generateDiv(numberSquares) {
-	removeAllChildNodes(container);
+	removeAllChildNodes(container); //Remove todas as divs do container para gerar um novo grid
 	for (let i = 0; i < numberSquares; i++) {
 		for (let j = 0; j < numberSquares; j++) {
 			let newDiv = document.createElement("div");
@@ -17,15 +18,21 @@ function generateDiv(numberSquares) {
 			container.appendChild(newDiv);
 		}
 	}
+	gridElements = document.querySelectorAll(".square");
+	setMouseOverEvent();
 }
-const gridElements = document.querySelectorAll(".square");
 
-gridElements.forEach((element) => {
-	element.addEventListener("mouseover", () => {
-		element.style.backgroundColor = "red";
+function setMouseOverEvent() {
+	//função que atribui a mudança de backgroud para cada div ao acionar o mouseOver
+	gridElements.forEach((element) => {
+		element.addEventListener("mouseover", () => {
+			element.style.backgroundColor = "red";
+		});
 	});
-});
-
+}
+gridSlider.onmouseup = function () {
+	generateDiv(this.value);
+};
 const newGridBtn = document.querySelector(".newGrid");
 newGridBtn.addEventListener("click", () => {
 	let value = prompt("Input new grid value");
